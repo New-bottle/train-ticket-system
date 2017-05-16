66967f84d81c896f188fe9278ab3bad0194d64ad
SupremeT <1012143610@qq.com>
2017-05-16 08:34:36 +0800

    ss

--- UI/searchbyline.cpp
+++ UI/searchbyline.cpp
@@ -3,6 +3,10 @@
 #include "usermainwindow.h"
 #include "../tts_server/header/query.h"
 #include "../tts_server/header/server.h"
+#include <QTableWidget>
+#include <QTableWidgetItem>
+
+extern sjtu::TTS tts;
 
 searchbyline::searchbyline(QWidget *parent) :
     QDialog(parent),
@@ -25,5 +29,15 @@ void searchbyline::on_pushButton_2_clicked()
 
 void searchbyline::on_pushButton_clicked()
 {
-
+    ui->tableWidget->clear();
+    sjtu::check_line_ans ans = tts.check_line(ui->train_name->text());
+    QTableWidgetItem one = new QTableWidgetItem("站名");
+    QTableWidgetItem two = new QTableWidgetItem("日期");
+    QTableWidgetItem three = new QTableWidgetItem("到达时间");
+    QTableWidgetItem one = new QTableWidgetItem("停车时间");
+    QTableWidgetItem one = new QTableWidgetItem("里程");
+    QTableWidgetItem one = new QTableWidgetItem("站名");
+    QTableWidgetItem one = new QTableWidgetItem("站名");
+    QTableWidgetItem one = new QTableWidgetItem("站名");
+    ui->tableWidget->setHorizontalHeaderItem();
 }
--- tts_server/source/query.cpp
+++ tts_server/source/query.cpp
@@ -2,11 +2,35 @@
 
 int sjtu::transfer_date(const QString & str)
 {
-    int ans = 0;
+    int ans = 0, line = 0;
     for(int i = 0; i < str.length(); ++i)
     {
         if(str[i] != '/')
             ans = ans * 10 + (str[i].unicode() - '0');
+        else
+        {
+            if(!line)
+            {
+                if(str[i + 1] == '1')
+                {
+                    if(str[i + 2] == '/') ans *= 10;
+                    else continue;
+                }
+                else ans *= 10;
+            }
+            else
+            {
+                if(i == str.length() - 2) ans *= 10;
+                else continue;
+            }
+        }
+        /*else if(str[i + 1] == '1')
+        {
+            if(str[i + 2] == '/')
+                ans *= 10;
+            else continue;
+        }
+        else ans *= 10;*/
     }
     return ans;
 }
